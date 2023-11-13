824a5f9a19d13f5470b36961f72df12d5126e7d5
glpat-q8s_hR65Pg98EqqCtRf- <glpat-q8s_hR65Pg98EqqCtRf->
2023-11-13 16:54:55 -0300

    feat sendMessage

--- popup.js
+++ popup.js
@@ -3,48 +3,59 @@ let btnChecked = false;
 let intervalChecked = 0;
 let contentText;
 
-document.addEventListener('DOMContentLoaded', function () {
-  var modifyButton = document.getElementById('modifyButton');
-  function timerMessages(status) {
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'voltaParaExtensao') {
+      setTimeout(() => {
+        timerMessages(true);
+      }, 2000);
+    }
+  });
+
+  var modifyButton = document.getElementById("modifyButton");
+  async function timerMessages(status) {
+    console.log("comecei denovo");
     if (status) {
-      intervalChecked = setInterval(async () => {
-        let item = await startProcess();
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: clickSend,
-          args: [item.id, item.number, item.message],
-        });
-      }, 5000);
+      let item = await startProcess();
+      let auth = localStorage.getItem("token");
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: clickSend,
+        args: [item.id, item.number, item.message, auth],
+      });
     } else {
-      clearInterval(intervalChecked);
       return;
     }
     function startProcess() {
       return new Promise(async (resolve) => {
         let [message] = await getMessages();
-        resolve(message)
-      })
+        resolve(message);
+      });
     }
     function getMessages() {
       return new Promise((resolve) => {
-        fetch('https://back.gdigital.com.br/wpp?status=preparing', {
-          method: 'GET',
+        fetch("https://back.gdigital.com.br/wpp?status=preparing", {
+          method: "GET",
           headers: {
-            'Authorization': localStorage.getItem('token'),
-            'Content-Type': 'application/json'
+            Authorization: localStorage.getItem("token"),
+            "Content-Type": "application/json",
           },
         })
           .then(async (response) => {
             let result = await response.json();
             if (!result.length) {
-              resolve(false)
+              resolve(false);
             } else {
               resolve(result);
             }
           })
-          .catch(() => {
-            resolve(false);
+          .catch(async () => {
+            let response = await getMessages();
+            resolve(response);
           });
       });
     }
@@ -52,123 +63,158 @@ document.addEventListener('DOMContentLoaded', function () {
     function startProcess() {
       return new Promise(async (resolve) => {
         let [message] = await getMessages();
-        console.log('busquei');
-        resolve(message)
-      })
+        // message.forEach(element => {
+        //   let item = document.createElement('p');
+        //   item.innerText = `${element.message} => ${element.number}`
+        //   document.querySelector('.automation-list').appendChild(item);
+        // });
+        resolve(message);
+      });
     }
 
     function getMessages() {
       return new Promise((resolve) => {
-        fetch('https://back.gdigital.com.br/wpp?status=preparing', {
-          method: 'GET',
+        fetch("https://back.gdigital.com.br/wpp?status=preparing", {
+          method: "GET",
           headers: {
-            'Authorization': localStorage.getItem('token'),
-            'Content-Type': 'application/json'
+            Authorization: localStorage.getItem("token"),
+            "Content-Type": "application/json",
           },
         })
           .then(async (response) => {
             let result = await response.json();
             if (!result.length) {
-              resolve(false)
+              resolve(false);
             } else {
               resolve(result);
             }
           })
-          .catch(() => {
-            resolve(false);
+          .catch(async () => {
+            let response = await getMessages();
+            resolve(response);
           });
       });
     }
-  };
+  }
 
-
-  modifyButton.addEventListener('click', async function () {
-    console.log('btn', btnChecked)
+  modifyButton.addEventListener("click", async function () {
+    console.log("btn", btnChecked);
     if (!btnChecked) {
       btnChecked = true;
-      modifyButton.style.background = 'red';
-      modifyButton.innerText = 'Parar'
+      modifyButton.style.background = "red";
+      modifyButton.innerText = "Parar";
       timerMessages(true);
     } else {
-      modifyButton.style.background = 'green';
-      modifyButton.innerText = 'Iniciar'
+      modifyButton.style.background = "green";
+      modifyButton.innerText = "Iniciar";
       btnChecked = false;
       timerMessages(false);
       return;
     }
   });
-  async function clickSend(id, number, message) {
+  async function clickSend(id, number, message, auth) {
     number = await removeCaracters(number);
     console.log(number, message);
-    let html = document.querySelector('body');
-    let newElement = document.createElement('a');
+
+    let html = document.querySelector("body");
+    let newElement = document.createElement("a");
     newElement.href = `https://wa.me/${number}?text=${message}`;
     html.appendChild(newElement);
+
+    window.debounceRequest = false;
+    window.debounceUrl = false;
+    document.addEventListener("DOMSubtreeModified", escutaBodyWrapper);
+
     newElement.click();
-    newElement.remove();
-    let response = await checkBtn();
-    console.log('resut message', response);
-    if (response) {
-      changeMessage(id, response)
+
+    function aaaaa() {
+        console.log('kaosdninauisdnisandonasoidnasodnsajkdnkasndkjsajdnsjkadnkasndkjanskjdnkjsdas');
     }
 
-    function changeMessage(id, result) {
-      fetch(`https://back.gdigital.com.br/wpp/changeStatusMessage?message_id=${id}&status=${result}`, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYmFjay5nZGlnaXRhbC5jb20uYnIvbG9naW4iLCJpYXQiOjE2OTk4MDgzNzgsImV4cCI6MTcwMTAxNzk3OCwibmJmIjoxNjk5ODA4Mzc4LCJqdGkiOiI1MHNwYUxNQ0N5M2RPYVNuIiwic3ViIjoiMTYzIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.0MkpyPRYAIhR6x3PIongLKlDDZQR0fEAWJzUouWHpxU`,
-          'Content-Type': 'application/json'
-        },
-      })
-        .then(async (response) => {
-          console.log('Suceeso', await response.json());
-        })
-        .catch((erro) => {
-          console.error('erro', erro);
-        });
+    function escutaBodyWrapper(e) {
+      escutaBody(e, id, auth);
     }
 
-    function checkBtn() {
-      return new Promise((resolve) => {
-        let countTentative = 0;
-        window.alert = function () { };
-        let timer = setInterval(() => {
-          let btn = document.querySelector('.tvf2evcx.oq44ahr5.lb5m6g5c.svlsagor.p2rjqpw5.epia9gcq');
-          if (btn) {
-            btn.click();
-            clearInterval(timer);
-            resolve('sent')
-          } else {
-            setTimeout(() => {
-              let msgError = document.querySelector('.emrlamx0.aiput80m.h1a80dm5.sta02ykp.g0rxnol2.l7jjieqr.hnx8ox4h.f8jlpxt4.l1l4so3b.le5p0ye3.m2gb0jvt.rfxpxord.gwd8mfxi.mnh9o63b.qmy7ya1v.dcuuyf4k.swfxs4et.bgr8sfoe.a6r886iw.fx1ldmn8.orxa12fk.bkifpc9x.rpz5dbxo.bn27j4ou.oixtjehm.hjo1mxmu.snayiamo.szmswy5k');
-              if (msgError) {
-                msgError.click()
-                resolve('cancelled')
-                clearInterval(timer);
-              }
-            }, 1000);
-            countTentative++
-            if (countTentative > 20) {
-              resolve('sent')
-            }
-            console.log('Buscando botão');
+    function escutaBody(e, id, auth) {
+      let btnSend = e.target.querySelector('[aria-label="Enviar"]');
+      let btnErro = e.target.querySelector(
+        ".emrlamx0.aiput80m.h1a80dm5.sta02ykp.g0rxnol2.l7jjieqr.hnx8ox4h.f8jlpxt4.l1l4so3b.le5p0ye3.m2gb0jvt.rfxpxord.gwd8mfxi.mnh9o63b.qmy7ya1v.dcuuyf4k.swfxs4et.bgr8sfoe.a6r886iw.fx1ldmn8.orxa12fk.bkifpc9x.rpz5dbxo.bn27j4ou.oixtjehm.hjo1mxmu.snayiamo.szmswy5y"
+      );
+
+      if (btnSend) {
+        let btnSendMessage = e.target.querySelector("._2xy_p._3XKXx > button");
+        setTimeout(async() => {
+          btnSendMessage.click();
+          document.removeEventListener("DOMSubtreeModified", escutaBodyWrapper);
+          let resp = await changeMessage(id, "sent", auth);
+          if(resp){
+            chrome.runtime.sendMessage({ action: 'voltaParaExtensao', dados: 'dados que você quer enviar' });
           }
-        }, 10);
-      });
+        }, 100);
+      } else if (btnErro) {
+        let btnError = e.target.querySelector("button");
+        setTimeout(async () => {
+          btnError.click();
+          document.removeEventListener("DOMSubtreeModified", escutaBodyWrapper);
+          let resp = await changeMessage(id, "error", auth);
+          if(resp){
+            chrome.runtime.sendMessage({ action: 'voltaParaExtensao', dados: 'dados que você quer enviar' });
+          }
+        }, 100);
+      } else {
+        return;
+      }
+      // else {
+      //   let textExist = document.querySelector('.selectable-text.copyable-text.iq0m558w.g0rxnol2 > .selectable-text.copyable-text');
+      //   if (!window.debounceUrl) {
+      //     window.debounceUrl = true;
+      //     setTimeout(() => {
+      //       if(!textExist){
+      //         message.click();
+      //       }
+      //       window.debounceUrl = false;
+      //     }, 1000);
+        // }
+
+      function changeMessage(id, result, auth) {
+        return new Promise((resultado) => {
+          if (!window.debounceRequest) {
+            window.debounceRequest = true;
+            fetch(
+              `https://back.gdigital.com.br/wpp/changeStatusMessage?message_id=${id}&status=${result}`,
+              {
+                method: "POST",
+                headers: {
+                  Authorization: auth,
+                  "Content-Type": "application/json",
+                },
+              }
+            )
+              .then(async () => {
+                resultado('sucesso');
+              })
+              .catch((erro) => {
+                resultado('error')
+              });
+            setTimeout(() => {
+              window.debounceRequest = false;
+            }, 1000);
+          } else {
+            return;
+          }
+        })
+      }
     }
 
     function removeCaracters(number) {
       return new Promise((resolve, reject) => {
-        let numeros = number.replace(/\D/g, '');
+        let numeros = number.replace(/\D/g, "");
         if (!/\d+/.test(numeros)) {
-          reject(new Error('Número inválido. Não contém dígitos.'));
+          reject(new Error("Número inválido. Não contém dígitos."));
           return;
         }
         resolve(numeros);
       });
     }
-
   }
-
 });
-
